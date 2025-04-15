315f4cbc5b6bf8ccaea2ac8d13af9e3eb339b3e9
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-04-15 18:30:44 +0100

    feat(tox): Configure tox tool. Add requirements files for testing and typing; update .gitignore and pyproject.toml

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,5 +1,6 @@
 from nb2prod_ml_model import predict
 
+
 def test_predict():
     result = predict.predict({}, input_data=[])
     assert "result" in result
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,5 +1,6 @@
 from nb2prod_ml_model import train
 
+
 def test_train_model():
     result = train.train_model(data=[])
     assert "model" in result
